57767458c5c9c489adf54d5296b55f00637648fe
Emran H Khan <emran.khan2400@gmail.com>
2021-04-07 05:34:40 +0600

    change in logic in orders comp.

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -28,7 +28,7 @@ const Orders = () => {
 
     return (
         <div>
-            <h1 className="text-center mt-3">You have ordered{orders.length ? orders.length : 'no'} products</h1>
+            <h1 className="text-center mt-3">You have ordered {orders.length ? orders.length : 'no'} products</h1>
             {
                 orders.map((order, index) =>
                     <div className="jumbotron w-50 mx-auto my-3 py-3 row" key={index}>
